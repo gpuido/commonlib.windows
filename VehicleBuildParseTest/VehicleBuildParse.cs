ca2d7e9088e5e4ceae7b0e4f4d54b178d647956b
sergey s prozhogin <sprozhogin@gmail.com>
2016-10-15 23:31:28 -0700

    added helper to recover build year from string

---
+++ VehicleBuildParseTest/VehicleBuildParse.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OasCommonLib.VinParsers;
+using System;
+
+namespace VehicleBuildParseTest
+{
+    [TestClass]
+    public class VehicleBuildParse
+    {
+        [TestMethod]
+        public void VehicleBuildParse_Test()
+        {
+            int build = BuildYearRecoverHelper.Recover("1");
+            Assert.AreEqual(build, 2001);
+
+            build = BuildYearRecoverHelper.Recover("19");
+            Assert.AreEqual(build, 1919);
+
+            int prevYear = DateTime.Now.Year - 1;
+            build = BuildYearRecoverHelper.Recover(prevYear.ToString());
+            Assert.AreEqual(build, prevYear);
+
+            build = BuildYearRecoverHelper.Recover("");
+            Assert.AreEqual(build, 2000);
+
+            build = BuildYearRecoverHelper.Recover("51");
+            Assert.AreEqual(build, 1951);
+        }
+    }
+}
